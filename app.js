d2707f270f90f5482f0c1806396ef551b4c0d1f4
William Xie <wxie6@illinois.edu>
2015-05-09 21:12:48 -0500

    Renamed public folder to frontend. Moved partials out one level up. This is for Passport integration for Sukeer.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/frontend'));
 
 var port = process.env.PORT || 3000;
 console.log("Express server running on " + port);
