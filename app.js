d7f9fad922e1718839661bb6b868a453980498f5
William Xie <wxie6@illinois.edu>
2015-05-09 23:07:06 -0500

    Reverted app.js to using public folder by default. Added login/register pages

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/frontend'));
+app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 3000;
 console.log("Express server running on " + port);
